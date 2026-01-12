7e1b666a8580f4fa22962f87405e3474a1cfcc0d
Jubayer Hasan <jubaihasan88@gmail.com>
2026-01-13 00:02:16 +0600

    added image data

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 
 export const metadata = {
   title: "PhotoFeed",
